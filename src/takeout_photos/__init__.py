8511d68173616d0a59767406472eb5a3aeae71c9
semantic-release <semantic-release>
2026-03-07 15:23:50 +0000

    1.0.3
    Automatically generated by python-semantic-release

--- src/takeout_photos/__init__.py
+++ src/takeout_photos/__init__.py
@@ -17,7 +17,7 @@ from takeout_photos.core.config import Config
 from takeout_photos.core.database import PipelineDB
 from takeout_photos.core.pipeline import Pipeline
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 __all__ = [
     "__version__",
